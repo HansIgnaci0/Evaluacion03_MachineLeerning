6ecbdf48d9c34c03d6b51824221276c35ac0fb5f
Hans Mancilla <ha.mancilla@duocuc.cl>
2025-11-27 16:39:16 -0300

    Delete covid19df directory

--- covid19df/src/covid19df/pipelines/clasificacion/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pipeline import create_classification_pipeline
-
-# Alias para Kedro
-create_pipeline = create_classification_pipeline
--- covid19df/src/covid19df/pipelines/reduccion_dimensional/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pipeline import create_pipeline
-
-# Alias para Kedro
-__all__ = ["create_pipeline"]
